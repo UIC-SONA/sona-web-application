1e19c6e40c6f7f0eb67c21067068f008070ae5d5
LUIDMIDEV <88100292+luidmidev@users.noreply.github.com>
2025-09-17 17:30:44 -0500

    FIX LOGGER

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,48 +1,39 @@
-const LEVEL = process.env.LOG_LEVEL || 'info'
+const LEVEL = (process.env.LOG_LEVEL || 'info').toLowerCase();
+const levels = ['trace', 'debug', 'info', 'warn', 'error'];
 
-const levelMatrix = [
-  ['trace', 'debug', 'info', 'warn', 'error'],
-  ['debug', 'info', 'warn', 'error'],
-  ['info', 'warn', 'error'],
-  ['warn', 'error'],
-  ['error']
-]
-
-const isTraceEnabled = levelMatrix[0].includes(LEVEL)
-const isDebugEnabled = levelMatrix[1].includes(LEVEL)
-const isInfoEnabled = levelMatrix[2].includes(LEVEL)
-const isWarnEnabled = levelMatrix[3].includes(LEVEL)
-const isErrorEnabled = levelMatrix[4].includes(LEVEL)
-
-const levelMap: Record<string, boolean> = {
-  trace: isTraceEnabled,
-  debug: isDebugEnabled,
-  info: isInfoEnabled,
-  warn: isWarnEnabled,
-  error: isErrorEnabled
+// Índice del nivel actual
+const currentLevelIndex = levels.indexOf(LEVEL);
+if (currentLevelIndex === -1) {
+  throw new Error(`Invalid LOG_LEVEL: ${LEVEL}`);
 }
+
+// Mapa de niveles habilitados
+const levelMap: Record<string, boolean> = {};
+levels.forEach((level, index) => {
+  levelMap[level] = index >= currentLevelIndex;
+});
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 const logger = {
   trace: (message: string, ...optionalParams: any[]) => {
-    if (isTraceEnabled) console.trace(`[TRACE] ${message}`, ...optionalParams)
+    if (levelMap.trace) console.trace(`[TRACE] ${message}`, ...optionalParams);
   },
   
   debug: (message: string, ...optionalParams: any[]) => {
-    if (isDebugEnabled) console.debug(`[DEBUG] ${message}`, ...optionalParams)
+    if (levelMap.debug) console.debug(`[DEBUG] ${message}`, ...optionalParams);
   },
   
   info: (message: string, ...optionalParams: any[]) => {
-    if (isInfoEnabled) console.info(`[INFO] ${message}`, ...optionalParams)
+    if (levelMap.info) console.info(`[INFO] ${message}`, ...optionalParams);
   },
   
   warn: (message: string, ...optionalParams: any[]) => {
-    if (isWarnEnabled) console.warn(`[WARN] ${message}`, ...optionalParams)
+    if (levelMap.warn) console.warn(`[WARN] ${message}`, ...optionalParams);
   },
   
   error: (message: string, ...optionalParams: any[]) => {
-    if (isErrorEnabled) console.error(`[ERROR] ${message}`, ...optionalParams)
+    if (levelMap.error) console.error(`[ERROR] ${message}`, ...optionalParams);
   },
   
   isLevelEnabled: (level: string) => levelMap[level] || false,
@@ -50,4 +41,4 @@ const logger = {
   level: LEVEL
 }
 
-export default logger
+export default logger;
